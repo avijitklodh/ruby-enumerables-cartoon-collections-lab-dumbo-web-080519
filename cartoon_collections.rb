86777eb49f50d96402a6bf2f052ae2b87d96132c
Avijit Lodh <avijitklodh@gmail.com>
2019-07-25 07:07:35 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,7 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves (names)
   # Your code here
+  
+  puts " #{names [i]} #{names} "
 end
 
 def summon_captain_planet# code an argument here
