5c84e621c879326a36aeedcddd8f193f5ccf0b5c
Avijit Lodh <avijitklodh@gmail.com>
2019-07-25 07:36:40 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,8 +8,13 @@ def roll_call_dwarves (names)
 
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(elements)
   # Your code here
+  a = []
+  i=0 
+  while i < elements.length do 
+    puts elements.capitalize
+  
 end
 
 def long_planeteer_calls# code an argument here
