bff6aa148dc42c118827f76a046c930135d1caf8
Avijit Lodh <avijitklodh@gmail.com>
2019-07-25 08:38:54 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,7 +14,7 @@ end
 
 def long_planeteer_calls(call)# code an argument here
   # Your code here
-  call.any? 
+  call.any? {|word| word.length > 4}
 end
 
 def find_the_cheese# code an argument here
