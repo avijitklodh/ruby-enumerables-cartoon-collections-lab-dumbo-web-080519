bf6cf2a59cc1e0cb405bf1e4622f7d5f7e45bc38
Avijit Lodh <avijitklodh@gmail.com>
2019-07-25 07:11:24 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,6 @@
 def roll_call_dwarves (names)
   # Your code here
-  %w(names).each_with_index 
-  puts " #{names [i]} #{names} "
+  %w(names).each_with_index {
 end
 
 def summon_captain_planet# code an argument here
